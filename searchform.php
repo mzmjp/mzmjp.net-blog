e0d7d311474fd64efb51b40dc49821629cb12d61
mzmjp <mzmjp.japan@gmail.com>
2013-11-08 22:08:54 +0900

    searchform.php を新たに作成して、音声検索できるようにした

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/wordpress/">
+				<div>
+					<label class="screen-reader-text" for="s">検索フォーム</label>
+					<input type="text" x-webkit-speech value="" name="s" id="s" />
+					<input type="submit" id="searchsubmit" value="検索する" />
+				</div>
+			</form>
