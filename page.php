ad24cadd5b685fbe61f748420544d54e6adfd9e5
mzmjp <mzmjp.japan@gmail.com>
2014-07-10 00:16:49 +0900

    font フォルダも .gitignore に追加

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+	<div class="mod-main">
+<?php if ( have_posts() ) : ?>
+<?php while (have_posts()) : the_post(); ?>
+		<article class="mod-entry">
+			<header class="mod-entry-header">
+				<h1 class="mod-entry-title"><?php the_title(); ?></h1>
+			<!-- /.mod-entry-header --></header>
+			<div class="mod-entry-content">
+<?php the_content(); ?>
+			<!-- /.mod-entry-content --></div>
+		<!-- /.mod-entry --></article>
+<?php endwhile; ?>
+	<!-- /.mod-main --></div>
+<?php else : ?>
+	<p>No pages exist.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
