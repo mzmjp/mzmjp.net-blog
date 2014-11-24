2134289cb3ac53d98dfce982328c1394fd4cd2d6
mzmjp <mzmjp.japan@gmail.com>
2014-11-24 16:19:06 +0900

    記事のタグ部分、マークアップ修正

--- archive.php
+++ archive.php
@@ -19,7 +19,7 @@ Template Name: Archives
 			<!-- /.mod-entry-content --></div>
 
 			<footer class="mod-entry-footer">
-				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname icon-tag">','</li><li class="mod-entry-tagname icon-tag">','</li></ul>');?>
+				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
 				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
 			<!-- /.mod-entry-footer --></footer>
 
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <?php the_content(); ?>
 			<!-- /.mod-entry-content --></div>
 			<footer class="mod-entry-footer">
-				<?php the_tags('<ul class="mod-entry-tags">Tags : <li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
+				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
 				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
 			<!-- /.mod-entry-footer --></footer>
 		<!-- /.mod-entry --></article>
