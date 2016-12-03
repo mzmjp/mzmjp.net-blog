9fd08b2b0b578fea4bd595dbc4d3419e862ada69
mzmjp <mzmjp.japan@gmail.com>
2016-12-04 00:11:27 +0900

    記事一覧ページの実装

--- archive.php
+++ archive.php
@@ -11,26 +11,38 @@ Template Name: Archives
 <?php while (have_posts()) : the_post(); ?>
 		<article class="mod-entry">
 			<header class="mod-entry-header">
-				<time class="mod-entry-date"><?php the_time("Y-m-d");?></time>
-				<h1 class="mod-entry-title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+				<h1 class="mod-entry-title"><?php the_title(); ?></h1>
 			<!-- /.mod-entry-header --></header>
-			<div class="mod-entry-content">
-<?php the_content(); ?>
-			<!-- /.mod-entry-content --></div>
 
-			<footer class="mod-entry-footer">
-				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
-				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
-			<!-- /.mod-entry-footer --></footer>
+			<div class="mod-entry-list">
+				<ol>
+				<?php
+					$args = array(
+						'posts_per_page'	=> -1,
+						'order'						=> 'DESC',
+						'orderby'					=> 'date',
+					);
+					$postslist = get_posts( $args );
+					foreach ( $postslist as $post ) :
+					setup_postdata( $post );
+				?>
+					<li>
+						<a href="<?php the_permalink(); ?>">
+							<div class="mod-entry-date">
+								<span class="icon-calendar" aria-hidden="true"></span><time><?php the_time("Y-m-d");?></time></div>
+								<?php the_title(); ?>
+						</a>
+					</li>
+				<?php
+					endforeach; 
+					wp_reset_postdata();
+				?>
+				</ol>
+			<!-- /.mod-entry-list --></div>
 
 		<!-- /.mod-entry --></article>
 <?php endwhile; ?>
-		<div class="mod-pager">
-			<ul>
-				<li><?php next_posts_link('&#x3c;&#x3c; 古い記事へ'); ?></li>
-				<li><?php previous_posts_link('新しい記事へ &#x3e;&#x3e;') ?></li>
-			</ul>
-		<!-- /.mod-pager --></div>
+
 	<!-- /.mod-main --></div>
 <?php else : ?>
 		<article class="mod-entry">
