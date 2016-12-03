b7b91ae91e7a8af67316b6964c041e7c1aaf6f57
mzmjp <mzmjp.japan@gmail.com>
2016-12-04 01:09:15 +0900

    各phpに404時の標示を追加

--- archive.php
+++ archive.php
@@ -13,7 +13,6 @@ Template Name: Archives
 			<header class="mod-entry-header">
 				<h1 class="mod-entry-title"><?php the_title(); ?></h1>
 			<!-- /.mod-entry-header --></header>
-
 			<div class="mod-entry-list">
 				<ol>
 				<?php
@@ -39,15 +38,28 @@ Template Name: Archives
 				?>
 				</ol>
 			<!-- /.mod-entry-list --></div>
-
 		<!-- /.mod-entry --></article>
+
 <?php endwhile; ?>
 
 	<!-- /.mod-main --></div>
+
 <?php else : ?>
+
 		<article class="mod-entry">
-			<p>No pages exist.</p>
+			<header class="mod-entry-header">
+				<div class="mod-entry-title">ページが見つかりませんでした。</div>
+			<!-- /.mod-entry-header --></header>
+			<div class="mod-entry-content">
+				<p>申し訳ございません、ページが見つかりませんでした。<br>ページが既に削除されてしまったか、URLが間違っている可能性があります。<br><a href="<?php bloginfo('url'); ?>/archive/">記事一覧</a>もしくは、<a href="#search">サイト内検索</a>からページをお探しください。</p>
+			<!-- /.mod-entry-content --></div>
+			<footer class="mod-entry-footer">
+				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
+				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
+			<!-- /.mod-entry-footer --></footer>
 		<!-- /.mod-entry --></article>
+	<!-- /.mod-main --></div>
+
 <?php endif; ?>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -11,10 +11,25 @@
 <?php the_content(); ?>
 			<!-- /.mod-entry-content --></div>
 		<!-- /.mod-entry --></article>
+
 <?php endwhile; ?>
+
 	<!-- /.mod-main --></div>
+
 <?php else : ?>
-	<p>No pages exist.</p>
+		<article class="mod-entry">
+			<header class="mod-entry-header">
+				<div class="mod-entry-title">ページが見つかりませんでした。</div>
+			<!-- /.mod-entry-header --></header>
+			<div class="mod-entry-content">
+				<p>申し訳ございません、ページが見つかりませんでした。<br>ページが既に削除されてしまったか、URLが間違っている可能性があります。<br><a href="<?php bloginfo('url'); ?>/archive/">記事一覧</a>もしくは、<a href="#search">サイト内検索</a>からページをお探しください。</p>
+			<!-- /.mod-entry-content --></div>
+			<footer class="mod-entry-footer">
+				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
+				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
+			<!-- /.mod-entry-footer --></footer>
+		<!-- /.mod-entry --></article>
+
 <?php endif; ?>
 
 <?php get_footer(); ?>
