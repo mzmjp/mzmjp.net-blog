d847b86c3a39232f42858bdb803fab7a3bb86db4
mzmjp <mzmjp.japan@gmail.com>
2017-01-03 22:46:43 +0900

    アーカイブのテンプレートをarchive.phpに変更

--- archive.php
+++ archive.php
@@ -1,48 +1,44 @@
-<?php
-/*
-Template Name: Archives
-*/
-?>
-
 <?php get_header(); ?>
 
 	<div class="mod-main" role="main">
+
+		<div class="mod-main-message"><p>
+		<?php
+		#タグページの場合
+		if ( is_tag() ) {
+			echo '<strong>';
+			echo single_tag_title();
+			echo '</strong>のタグが付けられている記事の一覧です。';
+		}
+		?>
+		</p></div><!-- /.mod-main-message -->
 <?php if ( have_posts() ) : ?>
 <?php while (have_posts()) : the_post(); ?>
 		<article class="mod-entry">
 			<header class="mod-entry-header">
-				<h1 class="mod-entry-title"><?php the_title(); ?></h1>
+				<div class="mod-entry-date"><span class="icon-calendar" aria-hidden="true"></span><time><?php the_time("Y-m-d");?></time></div>
+				<h1 class="mod-entry-title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 			<!-- /.mod-entry-header --></header>
-			<div class="mod-entry-list">
-				<ol>
-				<?php
-					$args = array(
-						'posts_per_page'	=> -1,
-						'order'						=> 'DESC',
-						'orderby'					=> 'date',
-					);
-					$postslist = get_posts( $args );
-					foreach ( $postslist as $post ) :
-					setup_postdata( $post );
-				?>
-					<li>
-						<div class="mod-entry-date"><span class="icon-calendar" aria-hidden="true"></span><time><?php the_time("Y-m-d");?></time></div>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</li>
-				<?php
-					endforeach; 
-					wp_reset_postdata();
-				?>
-				</ol>
-			<!-- /.mod-entry-list --></div>
+			<div class="mod-entry-content">
+			<?php the_content(); ?>
+			<!-- /.mod-entry-content --></div>
+			<footer class="mod-entry-footer">
+				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
+				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
+			<!-- /.mod-entry-footer --></footer>
 		<!-- /.mod-entry --></article>
-
 <?php endwhile; ?>
-
+		<div class="mod-pager">
+			<ul>
+				<li><?php next_posts_link('&#x3c;&#x3c; 古い記事へ'); ?></li>
+				<li><?php previous_posts_link('新しい記事へ &#x3e;&#x3e;') ?></li>
+			</ul>
+		<!-- /.mod-pager --></div>
 	<!-- /.mod-main --></div>
 
 <?php else : ?>
 
+	<div class="mod-main" role="main">
 		<article class="mod-entry">
 			<header class="mod-entry-header">
 				<div class="mod-entry-title">ページが見つかりませんでした。</div>
