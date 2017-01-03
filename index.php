6eadec5a6761e240191a572a382267b30f06da93
mzmjp <mzmjp.japan@gmail.com>
2017-01-03 22:47:34 +0900

    インデント修正

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 				<h1 class="mod-entry-title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 			<!-- /.mod-entry-header --></header>
 			<div class="mod-entry-content">
-<?php the_content(); ?>
+			<?php the_content(); ?>
 			<!-- /.mod-entry-content --></div>
 			<footer class="mod-entry-footer">
 				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
