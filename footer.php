8e69ca4c5f10ba45ccf3219a3df961d4ca2cd51b
mzmjp <mzmjp.japan@gmail.com>
2017-01-09 20:11:49 +0900

    トラッキングコードの位置を</body>直前から、</head>直前に変更

--- footer.php
+++ footer.php
@@ -54,18 +54,6 @@
 	});
 	</script>
 	<!-- /Menu Toggle -->
-	<!-- Google Analytics -->
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-29186068-1', 'mzmjp.net');
-	  ga('send', 'pageview');
-
-	</script>
-	<!-- /Google Analytics -->
 	<!-- Google Code Prettify -->
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/prettify.js"></script>
 	<script>$(function(){prettyPrint()});</script>
--- header.php
+++ header.php
@@ -10,6 +10,18 @@
 <meta name="description" content="<?php echo function_description(); ?>">
 <title><?php echo function_title(); ?></title>
 <?php wp_head(); ?>
+	<!-- Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-29186068-1', 'mzmjp.net');
+	  ga('send', 'pageview');
+
+	</script>
+	<!-- /Google Analytics -->
 </head>
 <body <?php body_class(); ?> onload="prettyPrint()">
 	<header class="mod-header" role="banner">
