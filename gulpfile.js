13b693a5eae1d96d2437df2a790bea78cb05095f
mzmjp <mzmjp.japan@gmail.com>
2018-08-15 14:42:45 +0900

    <head>内にスタイルを出力

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 var gulp = require('gulp');
 var compass = require('gulp-compass');
 var webserver = require('gulp-webserver');
+var fileinclude = require('gulp-file-include');
 
 gulp.task('webserver', function() {
-    gulp.src('')
-        .pipe(webserver({
-            livereload: true
-        }));
+  gulp.src('')
+  .pipe(webserver({
+    livereload: true
+  }));
 });
+
 gulp.task('compass', function () {
-    gulp.src('sass/*.scss')
-        .pipe(compass({
-            config_file: 'config.rb',
-            css: '',
-            sass: 'sass'
-        }))
-        .pipe(gulp.dest(''));
+  gulp.src('sass/*.scss')
+  .pipe(compass({
+    config_file: 'config.rb',
+    css: '',
+    sass: 'sass'
+  }))
+  .pipe(gulp.dest(''));
 });
+
+gulp.task('fileinclude', function() {
+  gulp.src('inc/header.php')
+  .pipe(fileinclude({
+    prefix: '@@',
+    basepath: '@file'
+  }))
+  .pipe(gulp.dest(''));
+});
+
 gulp.task('watch', function() {
-    gulp.watch('sass/*.scss', ['compass']);
+  gulp.watch('sass/*.scss', ['compass']);
+  gulp.watch('style.css', ['fileinclude']);
+  gulp.watch('inc/header.php', ['fileinclude']);
 });
-gulp.task('default', ['webserver', 'watch']);
+
+gulp.task('default', ['webserver', 'watch', 'fileinclude']);
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>@@include('../style.css')</style>
+<link rel="apple-touch-icon-precomposed" href="<?php bloginfo( 'template_url' ); ?>/favicon-152.png">
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> RSSフィード" href="<?php bloginfo( 'rss2_url' ); ?>">
+<?php echo function_noindex(); ?>
+<meta name="description" content="<?php echo function_description(); ?>">
+<title><?php echo function_title(); ?></title>
+<?php wp_head(); ?>
+	<!-- Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-29186068-1', 'mzmjp.net');
+	  ga('send', 'pageview');
+
+	</script>
+	<!-- /Google Analytics -->
+</head>
+<body <?php body_class(); ?> onload="prettyPrint()">
+	<header class="mod-header">
+		<div class="mod-title">
+			<div class="mod-title-inner">
+				<h1 class="mod-title-logo"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+				<a href="#menu" class="mod-menu-button" role="button" aria-controls="menu" aria-expanded="false"><span class="icon-menu" aria-hidden="true"></span>Menu</a>
+			<!-- /.mod-title-inner --></div>
+		<!-- /.mod-title-wrap --></div>
+		<div id="menu" class="mod-menu">
+			<div class="mod-menu-inner">
+				<div class="mod-menu-about">
+					<a href="<?php bloginfo('url'); ?>/about">About</a>
+				<!-- /.mod-menu-about --></div>
+				<div class="mod-menu-archive">
+					<a href="<?php bloginfo('url'); ?>/archive">Archive</a>
+				<!-- /.mod-menu-about --></div>
+				<div class="mod-menu-search" role="search">
+					<div class="mod-search-content">
+						<?php get_search_form(); ?>
+					<!-- /.mod-search-content --></div>
+				<!-- /#search --></div>
+			<!-- /.mod-menu-inner --></div>
+		<!-- /.mod-menu --></div>
+	<!-- /.mod-header --></header>
