7dc3c28f957b245289c117ca9a20422bbdef69b3
mzmjp <mzmjp.japan@gmail.com>
2019-08-04 18:00:21 +0900

    gulpを4系にするにともなった変更

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,29 @@ var gulp = require('gulp');
 var compass = require('gulp-compass');
 var fileinclude = require('gulp-file-include');
 
-gulp.task('compass', function () {
+gulp.task('compass', done => {
   gulp.src('sass/*.scss')
   .pipe(compass({
     config_file: 'config.rb',
     css: '',
     sass: 'sass'
   }))
-  .pipe(gulp.dest(''));
+  .pipe(gulp.dest('./'));
+  done();
 });
 
-gulp.task('fileinclude', function() {
+gulp.task('fileinclude', done => {
   gulp.src('inc/header.php')
   .pipe(fileinclude({
     prefix: '@@',
     basepath: '@file'
   }))
-  .pipe(gulp.dest(''));
+  .pipe(gulp.dest('./'));
+  done();
 });
 
-gulp.task('watch', function() {
+gulp.task('default', function() {
   gulp.watch('sass/*.scss', gulp.series('compass'));
   gulp.watch('style.css',  gulp.series('fileinclude'));
   gulp.watch('inc/header.php',  gulp.series('fileinclude'));
-});
-
-// gulp.task('default',
-//   gulp.parallel('watch', 'fileinclude')
-// );
-
-// gulp.task('default', gulp.task['watch', 'fileinclude']);
-
-gulp.task('default', (done) => {
-  gulp.task('watch');
-  gulp.task('fileinclude');
-  done();
 });
