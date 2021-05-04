00aadb72c87c43b9905d90841cf3e75dc37e3415
mzmjp <mzmjp.japan@gmail.com>
2021-05-04 17:07:14 +0900

    Dart Sassへ移行

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var fileinclude = require('gulp-file-include');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var fileinclude = require("gulp-file-include");
+sass.compiler = require("sass");
 
-gulp.task('sass', done => {
-  gulp.src('src/sass/*.scss')
-  .pipe(sass({
-    config_file: 'config.rb',
-    css: '',
-    sass: 'src/sass',
-    outputStyle: 'compressed',
-  }))
-  .pipe(gulp.dest('./'));
+gulp.task("sass", (done) => {
+  gulp
+    .src("src/sass/*.scss")
+    .pipe(
+      sass({
+        config_file: "config.rb",
+        css: "",
+        sass: "src/sass",
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(gulp.dest("./"));
   done();
 });
 
-gulp.task('fileinclude', done => {
-  gulp.src('src/header.php')
-  .pipe(fileinclude({
-    prefix: '@@',
-    basepath: '@file'
-  }))
-  .pipe(gulp.dest('./'));
+gulp.task("fileinclude", (done) => {
+  gulp
+    .src("src/header.php")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("./"));
   done();
 });
 
-gulp.task('default', function() {
-  gulp.watch('src/sass/*.scss', gulp.series('sass'));
-  gulp.watch('style.css',  gulp.series('fileinclude'));
-  gulp.watch('src/header.php',  gulp.series('fileinclude'));
+gulp.task("default", function () {
+  gulp.watch("src/sass/*.scss", gulp.series("sass"));
+  gulp.watch("style.css", gulp.series("fileinclude"));
+  gulp.watch("src/header.php", gulp.series("fileinclude"));
 });
