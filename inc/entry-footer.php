e607504bef7fabf7d269669a564ed3cce7b4f1bb
mzmjp <mzmjp.japan@gmail.com>
2022-04-03 21:42:43 +0900

    typo fixed

--- inc/entry-footer.php
+++ inc/entry-footer.php
@@ -2,5 +2,5 @@
 				<div class="mod-entry-tag">
 					<?php the_tags('<div class="mod-entry-tag-title">タグ：</div><ul class="mod-entry-tags"><li class="mod-entry-tagname">','</li><li class="mod-entry-tagname">','</li></ul>');?>
 				</div>
-				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">「<?php the_title_attribute(); ?>」のパーマリンク</a></p>
+				<p class="mod-entry-permalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">「<?php the_title_attribute(); ?>」のパーマリンク</a></p>
 			<!-- /.mod-entry-footer --></footer>
