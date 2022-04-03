592216fe89d404af16922544c26c52d824316405
mzmjp <mzmjp.japan@gmail.com>
2022-04-03 21:03:38 +0900

    記事のタグ一覧の前に、見出しを追加

--- inc/entry-footer.php
+++ inc/entry-footer.php
@@ -1,4 +1,6 @@
 			<footer class="mod-entry-footer">
-				<?php the_tags('<ul class="mod-entry-tags"><li class="mod-entry-tagname">','</li><li class="mod-entry-tagname"><span class="icon-tag" aria-hidden="true"></span>','</li></ul>');?>
-				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">パーマリンク</a></p>
+				<div class="mod-entry-tag">
+					<?php the_tags('<div class="mod-entry-tag-title">タグ：</div><ul class="mod-entry-tags"><li class="mod-entry-tagname">','</li><li class="mod-entry-tagname">','</li></ul>');?>
+				</div>
+				<p class="mod-entry-parmalink"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">「<?php the_title_attribute(); ?>」のパーマリンク</a></p>
 			<!-- /.mod-entry-footer --></footer>
